d86347bffab9414d7cf3f823b0fc49e1231bc1ba
balugaq <129668183+balugaq@users.noreply.github.com>
2026-08-20 22:35:10 +0800

    polish github workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "io.github.balugaq"
 val archiveName = "RykenSlimeCustomizer"
-version = "v3.1.5"
+version = "3.1.5"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
